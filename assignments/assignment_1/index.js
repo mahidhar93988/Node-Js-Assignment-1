fc8846ea2f6816f1457fa89348e6c83e0e6e8b9d
Reddy Mahidhar <54352527+mahidhar93988@users.noreply.github.com>
2022-01-07 18:19:13 +0530

    Update index.js

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,13 +1,24 @@
 function getNameFromCommandLine() {
+    let name = process.argv
+    len = name.length
+    return(name[len-1])
     // Write you code here, name should be taken as args in process.argv
 }
 
 function getNameFromEnv() {
+    envName=process.env.name
+    return envName
     // Write your code here
 }
 
 function getNameFromReadLine() {
-    // Write your code here
+    var readLine = require("readline");
+    var rl = readLine.createInterface(
+        process.stdin,process.stdout);
+        rl.question(" ",(age)=>{
+            console.log(age);
+            rl.close();
+        });
 }
 
 module.exports = {
